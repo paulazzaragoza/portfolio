de13e13d594b60736b974ed79aa1fcd284991d50
paulazzaragoza <pmzv9602@gmail.com>
2026-08-08 03:17:58 +0200

    arreglo de base url

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: 'https://paulazzaragoza.github.io/portfolio/',
+  base: '/portfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
