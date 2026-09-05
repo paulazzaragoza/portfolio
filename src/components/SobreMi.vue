b35ed0b21a35fab1c3a6bd75e5f266ea75e2dae8
paulazzaragoza <pmzv9602@gmail.com>
2026-09-05 13:49:56 +0200

    mejoras en la responsividad

--- src/components/SobreMi.vue
+++ src/components/SobreMi.vue
@@ -43,7 +43,7 @@ const { t } = useI18n()
 
 .card-container {
   width: 100%;
-  max-width: 75rem;
+  max-width: 65rem;
   min-height: 30rem;
   padding: 2rem;
 
--- src/components/TarjetaTrabajos.vue
+++ src/components/TarjetaTrabajos.vue
@@ -49,7 +49,7 @@ const { trabajoInfo } = defineProps({
 
 .card-container {
   width: 100%;
-  max-width: 60rem;
+  max-width: 65rem;
   min-height: 20rem;
   padding: 2rem;
   margin-bottom: 2rem;
